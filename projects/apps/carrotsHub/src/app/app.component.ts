2f9e240520ad1ffcd7defa23d52c28eb97e61c7b
Natalia Bandurova <natacsp@mail.ru>
2024-11-11 11:08:37 +0300

    feat: add ChangeDetectionStrategy.onPush to app component

--- projects/apps/carrotsHub/src/app/app.component.ts
+++ projects/apps/carrotsHub/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,6 +8,7 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './app.component.less',
     providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
