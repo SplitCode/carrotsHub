0465bda759f6a5173e1266e1202e1fdadb123b0f
Natalia Bandurova <natacsp@mail.ru>
2024-11-11 11:47:04 +0300

    feat: add scripts and format files

--- projects/stylelint.config.js
+++ projects/stylelint.config.js
@@ -1,10 +1,10 @@
 export default {
-  extends: 'stylelint-config-standard',
-  rules: {
-      'color-named': 'never',
-      'declaration-empty-line-before': 'never',
-      'no-duplicate-selectors': true,
-      'no-descending-specificity': true,
-      'rule-empty-line-before': 'always',
-  },
+    extends: 'stylelint-config-standard',
+    rules: {
+        'color-named': 'never',
+        'declaration-empty-line-before': 'never',
+        'no-duplicate-selectors': true,
+        'no-descending-specificity': true,
+        'rule-empty-line-before': 'always',
+    },
 };
