f419ead4897b1be57360330c98001ac6032b2a49
Natalia Bandurova <natacsp@mail.ru>
2024-11-12 02:13:22 +0300

    chore: setup husky

--- apps/carrotsHub/src/app/app.config.ts
+++ apps/carrotsHub/src/app/app.config.ts
@@ -1,18 +1,18 @@
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { TuiRootModule } from '@taiga-ui/core';
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
 import {
   ApplicationConfig,
   provideZoneChangeDetection,
   importProvidersFrom,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { appRoutes } from "./app.routes";
+import { provideFirebaseApp, initializeApp } from "@angular/fire/app";
+import { provideAnalytics, getAnalytics } from "@angular/fire/analytics";
+import { provideAuth, getAuth } from "@angular/fire/auth";
+import { provideFirestore, getFirestore } from "@angular/fire/firestore";
 
-import { environment } from '../environments/environment';
+import { environment } from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
