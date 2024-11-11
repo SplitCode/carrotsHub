7fafdbfa9e83c3de6b36de39676023c1058d189c
Natalia Bandurova <natacsp@mail.ru>
2024-11-11 20:53:17 +0300

    chore: add firebase hosting

--- projects/apps/carrotsHub/src/environments/environment.ts
+++ projects/apps/carrotsHub/src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
     apiKey: 'AIzaSyB_l7x7-ck9xNIMJAcy1QCbRCruIk2n3Xk',
     authDomain: 'carrot-s-hub.firebaseapp.com',
     projectId: 'carrot-s-hub',
-    storageBucket: 'carrot-s-hub.appspot.com',
+    storageBucket: 'carrot-s-hub.firebasestorage.app',
     messagingSenderId: '1047568718956',
-    appId: '1:1047568718956:web:4f51c7eca74fca9772cc49',
-    measurementId: 'G-XG80ZGVQJ7',
+    appId: '1:1047568718956:web:1bfe235de170648b72cc49',
+    measurementId: 'G-TDNSFVL3YL'
   },
 };
