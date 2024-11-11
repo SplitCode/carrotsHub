0e500d800a4f64f919c4eb477808cb2fd75ff968
Natalia Bandurova <natacsp@mail.ru>
2024-11-11 19:45:49 +0300

    chore: change firebase config

--- projects/apps/carrotsHub/src/app/app.config.ts
+++ projects/apps/carrotsHub/src/app/app.config.ts
@@ -20,29 +20,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     importProvidersFrom(TuiRootModule),
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirebaseApp(() => {
-      const app = initializeApp(environment.firebase);
-      console.log('Firebase App initialized:', app.name);
-      return app;
-    }),
-    provideAnalytics(() => {
-      const analytics = getAnalytics();
-      console.log('Firebase Analytics initialized:', analytics);
-      return analytics;
-    }),
-    provideAuth(() => {
-      const auth = getAuth();
-      console.log('Firebase Auth initialized:', auth);
-      return auth;
-    }),
-    provideFirestore(() => {
-      const firestore = getFirestore();
-      console.log('Firestore initialized:', firestore);
-      return firestore;
-    }),
-    // provideAnalytics(() => getAnalytics()),
-    // provideAuth(() => getAuth()),
-    // provideFirestore(() => getFirestore())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideAnalytics(() => getAnalytics()),
   ],
 };
