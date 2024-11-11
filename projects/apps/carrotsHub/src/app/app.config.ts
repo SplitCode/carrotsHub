78fdf6d815b727d274d39871182d12c287314532
Natalia Bandurova <natacsp@mail.ru>
2024-11-11 17:14:26 +0300

    chore: setup firebase (need to check)

--- projects/apps/carrotsHub/src/app/app.config.ts
+++ projects/apps/carrotsHub/src/app/app.config.ts
@@ -7,6 +7,12 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +20,29 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     importProvidersFrom(TuiRootModule),
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => {
+      const app = initializeApp(environment.firebase);
+      console.log('Firebase App initialized:', app.name);
+      return app;
+    }),
+    provideAnalytics(() => {
+      const analytics = getAnalytics();
+      console.log('Firebase Analytics initialized:', analytics);
+      return analytics;
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      console.log('Firebase Auth initialized:', auth);
+      return auth;
+    }),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      console.log('Firestore initialized:', firestore);
+      return firestore;
+    }),
+    // provideAnalytics(() => getAnalytics()),
+    // provideAuth(() => getAuth()),
+    // provideFirestore(() => getFirestore())
   ],
 };
---
+++ projects/apps/carrotsHub/src/environments/environment.ts
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_l7x7-ck9xNIMJAcy1QCbRCruIk2n3Xk',
+  authDomain: 'carrot-s-hub.firebaseapp.com',
+  projectId: 'carrot-s-hub',
+  storageBucket: 'carrot-s-hub.firebasestorage.app',
+  messagingSenderId: '1047568718956',
+  appId: '1:1047568718956:web:4f51c7eca74fca9772cc49',
+  measurementId: 'G-XG80ZGVQJ7'
+};
+
+export const environment = {
+  production: false,
+  firebase: firebaseConfig
+};
