4f681805a54d11553c78ce5a1fb66d3ceb6713fe
Natalia Bandurova <natacsp@mail.ru>
2024-11-11 11:07:05 +0300

    refactor: delete default code in app component

--- projects/apps/carrotsHub/src/app/app.component.ts
+++ projects/apps/carrotsHub/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
