35f318eef368184a8f8d350f7ae061ab35f7f1b8
Natalia Bandurova <natacsp@mail.ru>
2024-11-11 14:18:55 +0300

    chore: update linter

--- projects/apps/carrotsHub/src/app/app.component.ts
+++ projects/apps/carrotsHub/src/app/app.component.ts
@@ -1,22 +1,22 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 import {
   TuiRootModule,
   TuiDialogModule,
   TuiAlertModule,
   TUI_SANITIZER,
-} from "@taiga-ui/core";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+} from '@taiga-ui/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   imports: [RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
-  selector: "app-root",
-  templateUrl: "./app.component.html",
+  selector: 'app-root',
+  templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrl: "./app.component.less",
+  styleUrl: './app.component.less',
   providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {
-  title = "carrotsHub";
+  title = 'carrotsHub';
 }
--- projects/apps/carrotsHub/src/app/app.config.ts
+++ projects/apps/carrotsHub/src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { TuiRootModule } from "@taiga-ui/core";
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
 import {
   ApplicationConfig,
   provideZoneChangeDetection,
   importProvidersFrom,
-} from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { appRoutes } from "./app.routes";
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
