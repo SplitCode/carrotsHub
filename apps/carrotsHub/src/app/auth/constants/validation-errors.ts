3c73713b66e77512b6a1236d9f47f81daa47df6f
Natalia Bandurova <natacsp@mail.ru>
2024-11-12 19:25:08 +0300

    feat: add layout for login form

---
+++ apps/carrotsHub/src/app/auth/constants/validation-errors.ts
@@ -0,0 +1,10 @@
+export const VALIDATION_ERRORS = {
+  required: "Обязательное поле",
+  // minlength: ({ requiredLength }: { requiredLength: number }) =>
+  //   `Минимальная длина — ${requiredLength} символов`,
+  // maxlength: ({ requiredLength }: { requiredLength: number }) =>
+  //   `Максимальная длина — ${requiredLength} символов`,
+  // min: "Введите неотрицательное число",
+  // max: "Введите сумму не более 10 000 000",
+  // futureDate: "Дата не может быть из будущего",
+};
--- apps/carrotsHub/src/app/auth/pages/login-page/login-page.component.ts
+++ apps/carrotsHub/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,12 +1,87 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import {
+  TuiDataListWrapperModule,
+  TuiCheckboxLabeledModule,
+  TuiFieldErrorPipeModule,
+  TuiInputModule,
+  TuiInputPasswordModule,
+  TuiTextareaModule,
+  TUI_VALIDATION_ERRORS,
+} from "@taiga-ui/kit";
+import { TuiDestroyService } from "@taiga-ui/cdk";
+import {
+  TuiButtonModule,
+  TuiErrorModule,
+  TuiLinkModule,
+  TuiModeModule,
+  TuiTextfieldControllerModule,
+} from "@taiga-ui/core";
+import { Router, RouterLink } from "@angular/router";
+import { VALIDATION_ERRORS } from "../../constants/validation-errors";
 
 @Component({
   selector: "app-login-page",
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TuiDataListWrapperModule,
+    TuiInputModule,
+    TuiTextareaModule,
+    TuiButtonModule,
+    TuiCheckboxLabeledModule,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
+    TuiInputPasswordModule,
+    TuiTextfieldControllerModule,
+    RouterLink,
+    TuiLinkModule,
+    TuiModeModule,
+  ],
   templateUrl: "./login-page.component.html",
   styleUrl: "./login-page.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    TuiDestroyService,
+    {
+      provide: TUI_VALIDATION_ERRORS,
+      useValue: VALIDATION_ERRORS,
+    },
+  ],
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  readonly loading = signal(false);
+  private readonly router = inject(Router);
+
+  readonly loginForm: FormGroup = new FormGroup({
+    email: new FormControl<string>("", {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl<string>("", {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // eslint-disable-next-line no-console
+      console.log("Form Submitted", this.loginForm.value);
+    } else {
+      console.error("Form is invalid");
+    }
+  }
+}
