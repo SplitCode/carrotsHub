c318de9a61698e322e143ed618d63c74ae7c4438
Natalia Bandurova <natacsp@mail.ru>
2024-11-13 14:01:03 +0300

    feat: add logout button

--- apps/carrotsHub/src/app/home/home.component.ts
+++ apps/carrotsHub/src/app/home/home.component.ts
@@ -1,12 +1,24 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { TuiButtonModule } from "@taiga-ui/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../auth/services/auth.service";
+import { PageRoutes } from "../app.routes-path";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiButtonModule],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthService);
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate([PageRoutes.Login]);
+  }
+}
