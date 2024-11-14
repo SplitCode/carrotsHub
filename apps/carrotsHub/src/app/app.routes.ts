b5cb7a5b532bbf84ccc7e285d5cd4202111f53d3
Natalia Bandurova <natacsp@mail.ru>
2024-11-14 10:59:12 +0300

    feat: change custom auth guard to angular firebase

--- apps/carrotsHub/src/app/app.routes.ts
+++ apps/carrotsHub/src/app/app.routes.ts
@@ -1,25 +1,32 @@
 import type { Route } from "@angular/router";
+import { AuthGuard } from "@angular/fire/auth-guard";
 import { HomeComponent } from "./home/home.component";
 import { LoginPageComponent } from "./auth/pages/login-page/login-page.component";
 import { RegisterPageComponent } from "./auth/pages/register-page/register-page.component";
 import { PageRoutes } from "./app.routes-path";
-import { authGuard } from "./auth/guards/auth.guard";
+import {
+  redirectUnauthorizedToLogin,
+  redirectLoggedInToHome,
+} from "./auth/guards/auth.guard";
 
 export const appRoutes: Route[] = [
   {
     path: PageRoutes.Home,
     component: HomeComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: PageRoutes.Login,
     component: LoginPageComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome },
   },
   {
     path: PageRoutes.Register,
     component: RegisterPageComponent,
-    canActivate: [authGuard],
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome },
   },
   // {
   //   path: "**",
--- apps/carrotsHub/src/app/auth/guards/auth.guard.ts
+++ apps/carrotsHub/src/app/auth/guards/auth.guard.ts
@@ -1,35 +1,11 @@
-import { Router } from "@angular/router";
-import type {
-  ActivatedRouteSnapshot,
-  CanActivateFn,
-  RouterStateSnapshot,
-} from "@angular/router";
-import { inject } from "@angular/core";
-import type { Observable } from "rxjs";
-import { map } from "rxjs";
-import { AuthService } from "../services/auth.service";
+import {
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from "@angular/fire/auth-guard";
 import { PageRoutes } from "../../app.routes-path";
 
-export const authGuard: CanActivateFn = (
-  _route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
-): Observable<boolean> => {
-  const authService = inject(AuthService);
-  const router = inject(Router);
+export const redirectUnauthorizedToLogin = () =>
+  redirectUnauthorizedTo([PageRoutes.Login]);
 
-  return authService.isAuth().pipe(
-    map((isAuth) => {
-      const url = state.url.slice(1);
-
-      if (isAuth && (url === PageRoutes.Login || url === PageRoutes.Register)) {
-        router.navigate([PageRoutes.Home]);
-        return false;
-      }
-      if (!isAuth && url !== PageRoutes.Login && url !== PageRoutes.Register) {
-        router.navigate([PageRoutes.Login]);
-        return false;
-      }
-      return true;
-    })
-  );
-};
+export const redirectLoggedInToHome = () =>
+  redirectLoggedInTo([PageRoutes.Home]);
