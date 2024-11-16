ae656994df014ed99672d8ed500348dcbc61c6af
Natalia Bandurova <natacsp@mail.ru>
2024-11-16 17:46:50 +0300

    feat: add databaseURL to env

--- apps/carrotsHub/src/environments/environment.ts
+++ apps/carrotsHub/src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyB_l7x7-ck9xNIMJAcy1QCbRCruIk2n3Xk",
     authDomain: "carrot-s-hub.firebaseapp.com",
+    databaseURL: "https://carrot-s-hub-default-rtdb.firebaseio.com",
     projectId: "carrot-s-hub",
     storageBucket: "carrot-s-hub.firebasestorage.app",
     messagingSenderId: "1047568718956",
