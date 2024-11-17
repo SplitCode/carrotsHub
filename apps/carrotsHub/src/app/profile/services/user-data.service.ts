b3fa7b3c8ffb625f907f7b3d3381d6623f118d14
Natalia Bandurova <natacsp@mail.ru>
2024-11-17 14:16:21 +0300

    refactor: update promise to observable

--- apps/carrotsHub/src/app/profile/services/user-data.service.ts
+++ apps/carrotsHub/src/app/profile/services/user-data.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/compat/database";
-import type { Observable } from "rxjs";
+import { from, type Observable } from "rxjs";
 import type { UserData } from "../models/user-data.interface";
 
 @Injectable({
@@ -13,15 +13,15 @@ export class UserDataService {
     return this.db.object<UserData>(`users/${uid}`).valueChanges();
   }
 
-  setUserData(uid: string, userData: UserData): Promise<void> {
-    return this.db.object(`users/${uid}`).set(userData);
+  setUserData(uid: string, userData: UserData): Observable<void> {
+    return from(this.db.object(`users/${uid}`).set(userData));
   }
 
-  updateUserData(uid: string, userData: Partial<UserData>): Promise<void> {
-    return this.db.object(`users/${uid}`).update(userData);
+  updateUserData(uid: string, userData: Partial<UserData>): Observable<void> {
+    return from(this.db.object(`users/${uid}`).update(userData));
   }
 
-  deleteUserData(uid: string): Promise<void> {
-    return this.db.object(`users/${uid}`).remove();
+  deleteUserData(uid: string): Observable<void> {
+    return from(this.db.object(`users/${uid}`).remove());
   }
 }
