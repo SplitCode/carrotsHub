0ca1c32257118fd5272a1f29b9b4e18a5a6bb5de
Natalia Bandurova <natacsp@mail.ru>
2024-11-17 18:33:18 +0300

    feat: add pages layout

--- apps/carrotsHub/src/app/recipes/pages/recipe-detail/recipe-detail.component.ts
+++ apps/carrotsHub/src/app/recipes/pages/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import type { OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-recipe-detail",
@@ -9,4 +12,26 @@ import { CommonModule } from "@angular/common";
   styleUrl: "./recipe-detail.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecipeDetailComponent {}
+export class RecipeDetailComponent implements OnInit {
+  recipe: any;
+
+  private readonly apiId = "01ea880a";
+  private readonly apiKey = "daaed082eb9b2c1bba652577a2e3326e";
+
+  private readonly route = inject(ActivatedRoute);
+  private readonly http = inject(HttpClient);
+
+  ngOnInit() {
+    const recipeId = this.route.snapshot.paramMap.get("id");
+    if (recipeId) {
+      this.getRecipeDetail(recipeId);
+    }
+  }
+
+  getRecipeDetail(recipeId: string) {
+    const url = `https://api.edamam.com/search?r=${recipeId}&app_id=${this.apiId}&app_key=${this.apiKey}`;
+    this.http.get<any>(url).subscribe((data) => {
+      this.recipe = data[0];
+    });
+  }
+}
--- apps/carrotsHub/src/app/recipes/pages/recipes-page.component.ts
+++ apps/carrotsHub/src/app/recipes/pages/recipes-page.component.ts
@@ -1,12 +1,47 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import type { OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { debounceTime, switchMap } from "rxjs";
 
 @Component({
   selector: "app-recipes-page",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: "./recipes-page.component.html",
   styleUrl: "./recipes-page.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RecipesPageComponent {}
+export class RecipesPageComponent implements OnInit {
+  searchControl = new FormControl("");
+  recipes: any[] = [];
+
+  private readonly apiId = "01ea880a";
+  private readonly apiKey = "daaed082eb9b2c1bba652577a2e3326e";
+
+  private readonly http = inject(HttpClient);
+  private readonly router = inject(Router);
+
+  ngOnInit() {
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(500),
+        switchMap((query) => this.searchRecipes(query || ""))
+      )
+      .subscribe((data) => {
+        this.recipes = data.hits.map((hit: any) => hit.recipe);
+      });
+  }
+
+  searchRecipes(query: string) {
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${this.apiId}&app_key=${this.apiKey}`;
+    return this.http.get<any>(url);
+  }
+
+  openRecipeDetail(recipeUri: string) {
+    const recipeId = encodeURIComponent(recipeUri);
+    this.router.navigate(["/recipe", recipeId]);
+  }
+}
