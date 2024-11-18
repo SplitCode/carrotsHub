6e725d8a1b81e0773cb3be05f3f179d3b84b44fb
Natalia Bandurova <natacsp@mail.ru>
2024-11-18 23:31:40 +0300

    feat: add home page layout

--- apps/carrotsHub/src/app/home/home.component.ts
+++ apps/carrotsHub/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { TuiAvatarModule } from "@taiga-ui/kit";
 import { AuthService } from "../auth/services/auth.service";
 import { PageRoutes } from "../shared/constants/app.routes-path";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiAvatarModule],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
