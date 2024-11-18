d88e93a86a7fdee26d57ed2bab8ee7d87c4017ad
Natalia Bandurova <natacsp@mail.ru>
2024-11-18 17:55:22 +0300

    feat: add const for api url

--- apps/carrotsHub/src/app/api/services/edamam.service.ts
+++ apps/carrotsHub/src/app/api/services/edamam.service.ts
@@ -4,6 +4,7 @@ import { map, type Observable } from "rxjs";
 import { environment } from "../../../environments/environment";
 import type { RecipeResponse } from "../models/edamam.interface";
 import type { RecipeDetail } from "../../recipes/models/recipe-detail.interface";
+import { API_URL } from "../constants/api.constants";
 
 @Injectable({
   providedIn: "root",
@@ -14,7 +15,7 @@ export class EdamamService {
   private readonly http = inject(HttpClient);
 
   searchRecipes(query: string): Observable<RecipeResponse> {
-    const url = "https://api.edamam.com/api/recipes/v2";
+    // const url = "https://api.edamam.com/api/recipes/v2";
     const params = new HttpParams()
       .set("q", query)
       .set("app_id", this.apiId)
@@ -25,11 +26,12 @@ export class EdamamService {
       "Edamam-Account-User": this.apiId,
     });
 
-    return this.http.get<RecipeResponse>(url, { headers, params });
+    return this.http.get<RecipeResponse>(API_URL, { headers, params });
   }
 
   getRecipeDetail(recipeId: string): Observable<RecipeDetail> {
-    const url = `https://api.edamam.com/api/recipes/v2/${recipeId}`;
+    // const url = `https://api.edamam.com/api/recipes/v2/${recipeId}`;
+    const url = `${API_URL}/${recipeId}`;
 
     const params = new HttpParams()
       .set("app_id", this.apiId)
