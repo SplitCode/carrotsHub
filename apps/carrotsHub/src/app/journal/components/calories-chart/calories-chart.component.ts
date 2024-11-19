0d7ca0d0dcad7c17050d543df5453d5ca14a99e3
Natalia Bandurova <natacsp@mail.ru>
2024-11-19 16:59:41 +0300

    feat: add data from db

--- apps/carrotsHub/src/app/journal/components/calories-chart/calories-chart.component.ts
+++ apps/carrotsHub/src/app/journal/components/calories-chart/calories-chart.component.ts
@@ -1,6 +1,15 @@
-import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import type { OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TuiRingChartModule } from "@taiga-ui/addon-charts";
+import { AuthService } from "../../../auth/services/auth.service";
+import { UserDataService } from "../../../profile/services/user-data.service";
+import type { UserData } from "../../../profile/models/user-data.interface";
 
 @Component({
   selector: "app-calories-chart",
@@ -10,9 +19,36 @@ import { TuiRingChartModule } from "@taiga-ui/addon-charts";
   styleUrl: "./calories-chart.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CaloriesChartComponent {
-  readonly caloriesMax = signal(2000); // данные из профиля
-  readonly caloriesConsumed = signal(1200); // потребленные калории
+export class CaloriesChartComponent implements OnInit {
+  readonly caloriesMax = signal<number>(2000);
+  readonly caloriesConsumed = signal<number>(200); // 0 по умолчанию
+  readonly protein = signal<number>(0);
+  readonly fat = signal<number>(0);
+  readonly carbohydrates = signal<number>(0);
+
+  private readonly authService = inject(AuthService);
+  private readonly userDataService = inject(UserDataService);
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe((user) => {
+      if (user) {
+        this.loadUserData(user.uid);
+      }
+    });
+  }
+
+  loadUserData(uid: string) {
+    this.userDataService
+      .getUserData(uid)
+      .subscribe((userData: UserData | null) => {
+        if (userData) {
+          this.caloriesMax.set(userData.calculatedCalories ?? 2000);
+          this.protein.set(userData.protein ?? 0);
+          this.fat.set(userData.fat ?? 0);
+          this.carbohydrates.set(userData.carbohydrates ?? 0);
+        }
+      });
+  }
 
   get caloriesLeft(): number {
     return this.caloriesMax() - this.caloriesConsumed();
